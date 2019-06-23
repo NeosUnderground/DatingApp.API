0fd248f213aa492f9d929ebb51f1c9b76cb0390d
NeosUnderground <33915360+NeosUnderground@users.noreply.github.com>
2019-06-23 02:26:22 -0500

    global error handling

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -29,7 +29,7 @@ namespace DatingApp.API.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
-            //validata request
+            //validate request
 
 
             //checks if user already exists, if not create new user
@@ -51,6 +51,7 @@ namespace DatingApp.API.Controllers
           [HttpPost("login")]
           public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
           {
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
               if(userFromRepo == null)
